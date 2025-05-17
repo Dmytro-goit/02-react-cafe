81eb3c05126c326a8ebbd2cc2e5daae6d5715993
Dmytro-goit <dmytro.moiyseev@gmail.com>
2025-05-17 22:56:13 +0300

    -

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
-import css from './App.module.css'
+import css from '../App/App.module.css'
 import CafeInfo from '../CafeInfo/CafeInfo';
 import type { Votes, VoteType } from '../../types/votes';
-import VoteOptions from '../VoteOptions/VoteOptoins';
+import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
-import Notification from '../Notification/Notifіcation';
+import Notification from '../Notification/Notification';
 import  {useState} from 'react';
 
 
@@ -15,14 +15,14 @@ function App() {
   }) 
 
  
-const handleVote = (type: VoteType) => {
+const handleVote: (type: VoteType) => void = (type) =>{
   setVotes(prev => ({
     ...prev,
     [type]: prev[type] + 1,
   }));
 };
 
-const resetVotes = () => {
+const resetVotes: () => void = () =>{
   setVotes({ good: 0, neutral: 0, bad: 0 });
 };
   
